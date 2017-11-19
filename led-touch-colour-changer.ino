b4994f28c956f5c327608df164ded645310c1fce
John Webster <jebster15@gmail.com>
2017-11-19 18:39:45 -0500

    updated, cleaned code

--- led-touch-colour-changer.ino
+++ led-touch-colour-changer.ino
@@ -7,7 +7,7 @@ int g = 3;
 int b = 4;
 int touch = 0;
 int counter = 0;
-bool busy = 0;
+//bool busy = 0;
 
 
 void setup() {
@@ -17,14 +17,13 @@ void setup() {
   pinMode(b, OUTPUT);
   pinMode(A1, INPUT);
   Serial.begin(9600);
-  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   touch = analogRead(A1);
-  
   Serial.println(touch);
+  
   if (touch){
     counter = counter + 1;
     Serial.println(counter);
@@ -35,39 +34,27 @@ void loop() {
   }
   
   if (counter == 0){ //red
-    digitalWrite(r, HIGH);
-    digitalWrite(g, LOW);
-    digitalWrite(b, LOW);
+    red();
   } else if (counter == 1){ //blue
-    digitalWrite(b, HIGH);
-    digitalWrite(r, LOW);
-    digitalWrite(g, LOW);
+    blue();  
   } else if (counter == 2){ //green
-    digitalWrite(g, HIGH);
-    digitalWrite(r, LOW);
-    digitalWrite(b, LOW);
+    green();
   } else if (counter == 3){ //white
     digitalWrite(r, HIGH);
     digitalWrite(g, HIGH);
     digitalWrite(b, HIGH);
   } else{
-    seizure(50);
+    party(50);
   }
   
- 
-delay(10);
+  delay(10);
   if (counter > 3){
     counter = 0;
   }
- // delay(1000);
-  
-//  digitalWrite(r, LOW);
-//  digitalWrite(g, LOW);
-//  digitalWrite(b, LOW);
 }
 
 
-bool seizure(int timer){
+bool party(int timer){
   int len = timer;
   while (len){
     red();
